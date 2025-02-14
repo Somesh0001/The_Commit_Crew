f032a6f12b00048155a057842f7a69f8068c2f13
Somesh Dwivedi <sd10072004@gmail.com>
2025-02-14 23:57:15 +0530

    adding toasts

--- app/components/DashboardComponents/DashboardSidebar.tsx
+++ app/components/DashboardComponents/DashboardSidebar.tsx
@@ -11,7 +11,6 @@ import {
 } from "@tabler/icons-react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 interface LinkType {

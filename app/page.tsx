9690ec736af9c831f5689df255d0feda01380a77
unnati <unnatichaurasia12@gmail.com>
2025-02-14 15:23:56 +0530

    ss

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,13 @@ export default async function Home() {
   const session = await getServerSession(options)
 
   return (
-    <>
+    <div className="flex flex-col w-full ">
+    <NavbarWrapper/> 
       {session ? (
         <UserCard user={session?.user} pagetype={"Home"} />
       ) : (
         <h1 className="text-5xl">You Shall Not Pass!</h1>
       )}
-    </>
+    </div>
   )
 }
